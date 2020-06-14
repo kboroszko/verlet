2d3cc82b90156b7ed9ca2eb0ffbf301f053d93ca
kboroszko <kajetan.boroszko@gmail.com>
2020-06-14 16:24:51 +0200

    hello world mpi

---
+++ main_par.cpp
@@ -0,0 +1,65 @@
+//
+// Created by kajetan on 14.06.2020.
+//
+
+
+
+
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <mpi.h>
+#include <iostream>
+
+#define MSG_PREFIX "Hello world from process "
+#define MSG_FORMAT "%4d"
+#define MSG_SUFFIX "!"
+
+int main(int argc, char * argv[])
+{
+    int myProcessNo;
+    int numProcesses;
+    char message[256];
+    int srcProcessNo = 0;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myProcessNo);
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
+
+    if (myProcessNo == 0)
+    {
+        /* Proces wysylajacy komunikat "Hello world..." */
+        int messageLen = snprintf(
+                message,
+                sizeof(message) / sizeof(char),
+                MSG_PREFIX MSG_FORMAT MSG_SUFFIX,
+                myProcessNo
+        ) + 1;
+        assert(messageLen <= sizeof(message) / sizeof(char));
+        assert(messageLen == strlen(MSG_PREFIX) + 4 + strlen(MSG_SUFFIX) + 1);
+        MPI_Bcast(
+                message,
+                messageLen,
+                MPI_CHAR,
+                srcProcessNo,
+                MPI_COMM_WORLD
+        );
+    }
+    else
+    {
+        /* Proces odbierajacy komunikaty "Hello world..." */
+        MPI_Bcast(
+                message,
+                strlen(MSG_PREFIX) + 4 + strlen(MSG_SUFFIX) + 1,
+                MPI_CHAR,
+                srcProcessNo,
+                MPI_COMM_WORLD
+        );
+        printf("Process %d received broadcast message \"%s\"\n", myProcessNo, message);
+    }
+
+//    std::cout << "hello\n";
+    MPI_Finalize();
+
+    return 0;
+}
